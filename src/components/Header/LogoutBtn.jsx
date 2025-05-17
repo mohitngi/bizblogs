53684c86f231d91feedb777b0ac517431e246f2d
Mohit Negi <mohitnegi2195@gmail.com>
2025-05-17 14:06:54 +0530

    Refactor LogoutBtn component

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import {useDispatch} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import authService from '../../appwrite/auth'
 import {logout } from '../../store/authSlice'
 
 function LogoutBtn() {
     const dispatch = useDispatch()
-    const logoutHandler = () => {
-        authService.logout().then(() => {
+    const navigate = useNavigate()
+
+    const logoutHandler = async () => {
+        try {
+            await authService.logout()
             dispatch(logout())
-            window.location.reload();
-        })
+            navigate('/')
+        } catch (error) {
+            console.error("Logout failed:", error)
+        }
     }
-  return (
-   <button className='inline-block px-6 py-2 duration-200 hover:bg-slate-400 rounded-full cursor-pointer' onClick={logoutHandler}>Logout</button>
-  )
+
+    return (
+        <button 
+            className='inline-block px-6 py-2 duration-200 hover:bg-slate-400 rounded-full cursor-pointer' 
+            onClick={logoutHandler}
+        >
+            Logout
+        </button>
+    )
 }
 
 export default LogoutBtn
